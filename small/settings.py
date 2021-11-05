e571d56f09166f44e4760809dfdcbccf393309f9
hayoondevil <hayoondevil@gmail.com>
2021-11-06 03:35:12 +0900

    RDS connection

--- small/settings.py
+++ small/settings.py
@@ -80,8 +80,16 @@ WSGI_APPLICATION = 'small.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'django',
+        'USER' : 'admin',
+        'PASSWORD' : 'wjdgkdbs',
+        'HOST' : 'database-2.cyiu6hunwxv5.ap-northeast-2.rds.amazonaws.com',
+        'PORT' : '3306',
+        'OPTIONS' : {
+            'init_command' : "SET sql_mode='STRICT_TRANS_TABLES'"
+        }
+
     }
 }
 
